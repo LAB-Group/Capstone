b584f915eb7249590d845f5c5e02e3172e28103a
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-07-17 22:29:00 -0400

    Profile page update

--- gameon-ui/src/components/ProfilePage/ProfilePage.jsx
+++ gameon-ui/src/components/ProfilePage/ProfilePage.jsx
@@ -1,34 +1,31 @@
 import * as React from "react"
-
+import { Container, Box, Flex, Image, Center, Input, Text, Square, HStack } from "@chakra-ui/react"
+import profile from "../../media/elmo-burning.gif"
 
 export default function ProfilePage(){
     return(
-        <div className="profile-page">
-            <div className="profile-info">
-                <img src="" alt="" />
-                <input type="text" className="name"/>
-                <input type="text" className="preferred"/>
-                <input type="text" className="location"/>
-                <input type="text" className="games-played"/>
-                <button className="edit-button">Edit Profile</button>
-            </div>
-            <div className="events-info">
-                <img src="" alt="" />
-                <input type="text" className="name"/>
-                <input type="text" className="preferred"/>
-                <input type="text" className="location"/>
-                <input type="text" className="games-played"/>
-                <button className="edit-button">Edit Profile</button>
-            </div>
-            <div className="profile-info">
-                <img src="" alt="" />
-                <input type="text" className="name"/>
-                <input type="text" className="preferred"/>
-                <input type="text" className="location"/>
-                <input type="text" className="games-played"/>
-                <button className="edit-button">Edit Profile</button>
-            </div>
-
-        </div>
+        <Container maxW="900px" maxH="1200px" mt="200px">
+           <Flex color='white'>
+                <Box flexWrap='wrap' boxSize='400px' minW='400px'>
+                    <Image 
+                        boxSize='200px'
+                        src={profile}/>
+                </Box>
+                <Box bg='#d6bcfa'>
+                {/* Spacing the Name from the Handle */}
+                    <HStack spacing='15em'>
+                        <Text>Name of User</Text>
+                        <Text>Preferred Handle</Text>
+                    </HStack>
+                    {/* Centered and spaced from the above text */}
+                    <Center pt="2em">
+                        <Text>Location</Text>
+                    </Center>
+                    <Center pt="3em">
+                        <Text>Games Played</Text>
+                    </Center>
+                </Box>
+            </Flex>
+        </Container>
     )
 }
