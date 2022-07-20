329b3e89d495201448e5cb17c3d8bfd71a94e972
aangelo742 <83144033+aangelo742@users.noreply.github.com>
2022-07-20 14:16:42 -0500

    Added cors-anywhere server that allows idgb calls on frontend

--- gameon-api/server.js
+++ gameon-api/server.js
@@ -1,5 +1,18 @@
 const app = require("./app")
 const { PORT } = require("./config")
+var cors_anywhere = require("cors-anywhere")
+
+const proxyPort = 4001
+
+// installed npm install --save cors-anywhere, also installed apicalypse but never used it, might be an issue?
+cors_anywhere = require('cors-anywhere')
+cors_anywhere.createServer({
+    originWhiteList: [],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(proxyPort, function() {
+    console.log(`🚀 Running CORS Anywhere on localhost:${proxyPort}`)
+})
 
 app.listen(PORT, () => {
     console.log(`🐱‍👤Server running http://localhost:${PORT}`)
