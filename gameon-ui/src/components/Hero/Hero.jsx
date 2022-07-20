b8336657583f8fc711001cd30f36e77e93ebcd5a
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-07-20 15:25:43 -0400

    switching branch

--- gameon-ui/src/components/Hero/Hero.jsx
+++ gameon-ui/src/components/Hero/Hero.jsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Heading, Text } from "@chakra-ui/react"
 import { Slide } from "react-slideshow-image"
 import pic from "../../media/evo.jpg"
 import { Container, Box, Image } from '@chakra-ui/react'
@@ -15,10 +14,10 @@ export default function Hero() {
 }
 
 function Slideshow () {
-    const blazBlue = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dcead6af-f80b-434a-95ff-e4f642e8da10/del9x8v-a10a328a-386b-49b5-b616-82cee562b577.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZWFkNmFmLWY4MGItNDM0YS05NWZmLWU0ZjY0MmU4ZGExMFwvZGVsOXg4di1hMTBhMzI4YS0zODZiLTQ5YjUtYjYxNi04MmNlZTU2MmI1NzcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.-cwuEFvPq_T4WELqZujyeoQtUtdqodsVhVB5e2o0Q1U'
-    const tekken = 'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_auto/ncom/en_US/games/switch/b/blazblue-cross-tag-battle-switch/hero'
+    const guiltyGear = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dcead6af-f80b-434a-95ff-e4f642e8da10/del9x8v-a10a328a-386b-49b5-b616-82cee562b577.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZWFkNmFmLWY4MGItNDM0YS05NWZmLWU0ZjY0MmU4ZGExMFwvZGVsOXg4di1hMTBhMzI4YS0zODZiLTQ5YjUtYjYxNi04MmNlZTU2MmI1NzcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.-cwuEFvPq_T4WELqZujyeoQtUtdqodsVhVB5e2o0Q1U'
+    const blazBlue = 'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_auto/ncom/en_US/games/switch/b/blazblue-cross-tag-battle-switch/hero'
     const slideImages = [
-        pic,blazBlue,tekken
+        pic,guiltyGear,blazBlue
     ]
     return (
 
@@ -34,7 +33,7 @@ function Slideshow () {
                     bgRepeat="no-repeat"
                 >
 
-                    <Image src={slideImage} key={idx} objectFit='cover' width={"100%"} height={"100%"} borderRadius="lg"/>
+                    <Image src={slideImage} key={idx} width={"100%"} height={"100%"} borderRadius="lg"/>
                 </Box>
                 ) 
                 )}
