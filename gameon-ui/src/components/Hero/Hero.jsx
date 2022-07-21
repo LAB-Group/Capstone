01d26a65d322c1fab36a152d2115c38a65d7eed4
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-07-21 17:03:01 -0400

    updated home and hero

--- gameon-ui/src/components/Hero/Hero.jsx
+++ gameon-ui/src/components/Hero/Hero.jsx
@@ -1,19 +1,22 @@
 import * as React from "react"
 import { Slide } from "react-slideshow-image"
+// import { Swiper, SwiperSlide } from "swiper/react"
 import pic from "../../media/evo.jpg"
-import { Container, Box, Image } from '@chakra-ui/react'
+import { Container, Box, Image, Text, Flex } from '@chakra-ui/react'
 import "react-slideshow-image/dist/styles.css"
-
 export default function Hero() {
     return(
         // Shrinks container to 480px currently
         <Container maxW="3600px" minW="480px">
-            <Slideshow/>
+            <Flex flexDirection="column" alignItems="center">
+                <Slideshow/> 
+            </Flex> 
         </Container>
     )
 }
 
 function Slideshow () {
+    
     const guiltyGear = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dcead6af-f80b-434a-95ff-e4f642e8da10/del9x8v-a10a328a-386b-49b5-b616-82cee562b577.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2RjZWFkNmFmLWY4MGItNDM0YS05NWZmLWU0ZjY0MmU4ZGExMFwvZGVsOXg4di1hMTBhMzI4YS0zODZiLTQ5YjUtYjYxNi04MmNlZTU2MmI1NzcuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.-cwuEFvPq_T4WELqZujyeoQtUtdqodsVhVB5e2o0Q1U'
     const blazBlue = 'https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_auto/ncom/en_US/games/switch/b/blazblue-cross-tag-battle-switch/hero'
     const slideImages = [
@@ -21,7 +24,8 @@ function Slideshow () {
     ]
     return (
 
-
+        
+        //   Old Slideshow code
         // Widen container's width 
         <Container maxW="3600px" minW="480px" >
             <Slide width="100%">
@@ -39,6 +43,16 @@ function Slideshow () {
                 )}
             </Slide>
         </Container>
+
+        //Learning Swiper 
+    //     <>
+    //     <Swiper className="mySwiper">
+    //             <SwiperSlide><Image src={slideImages[0]}/></SwiperSlide>
+    //             <SwiperSlide><Image src={slideImages[1]}/></SwiperSlide>
+    //             <SwiperSlide><Image src={slideImages[2]}/></SwiperSlide>
+    //     </Swiper>
+    //   </>
+
 
     )
 }
