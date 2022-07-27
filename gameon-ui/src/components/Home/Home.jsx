8e84fbce86122be61376f486aa955866d61099ff
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-07-27 13:43:54 -0400

    fixed image in event details

--- gameon-ui/src/components/Home/Home.jsx
+++ gameon-ui/src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import * as React from "react"
 import Hero from "../Hero/Hero"
 import EventFeed from "../Events/EventFeed"
-import { Image, Container, Spacer } from "@chakra-ui/react"
+import { Image, Container, Spacer, Box } from "@chakra-ui/react"
 
 export default function Home(){
     return (
     
-            <Container centerContent minWidth="95vw" >
+            <Container centerContent minWidth="100vw">
+
                 <Hero />
                 <Spacer/>
                 <EventFeed />
