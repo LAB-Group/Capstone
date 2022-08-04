faa593fbe6e1b82b3ad2a71e005b929cae01ed08
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-08-04 01:02:38 -0400

    Edit font in posts and comments components

--- gameon-ui/src/components/Posts/PostsFeed.jsx
+++ gameon-ui/src/components/Posts/PostsFeed.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import apiClient from '../../services/apiClient';
-import { Text, Container, VStack, Stack, Heading, Box } from '@chakra-ui/react';
+import { Text, Container, HStack, VStack, Stack, Heading, Box } from '@chakra-ui/react';
 import Posts from './Posts';
 import { COLORS } from "../colors"
 
@@ -9,22 +9,42 @@ export default function PostsFeed({ eventId, posts }) {
 
 
   return (
-    <>
-    <VStack>
-    <Stack display={'flex'} justifyContent='flex-start'>
-      <Text alignItems={"left"} position={"relative"} fontSize={"3xl"} fontWeight={300} textTransform={'uppercase'} mb={4}>Comments</Text>
-    </Stack>
+    <Box position={"relative"} py={"25px"}>
+        <Stack spacing={{ base: 4, sm: 6 }} direction={"column"}>
+        <Box textAlign={"left"} mx={5}>
+      <Text marginLeft={5} fontFamily={"Roboto, sans-serif"} alignItems={"left"} position={"relative"} fontSize={"3xl"} fontWeight={300} textTransform={'uppercase'} mb={4}>Comments</Text>
+      </Box>
+      </Stack>
       <VStack>
-        {posts?.map((post, index) => (
+         {posts?.map((post, index) => (
           <Posts key={index} post={post} eventId={eventId} />
         ))}
-        {/* {posts?.length ? (
+        {posts?.length ? (
           <Box>
-            <Text>No Posts available</Text>
+            <Text fontFamily={"mono, sans-serif"}>No Posts available</Text>
           </Box>
-        ) : null} */}
+        ) : null}
       </VStack>
-    </VStack>
-    </>
+    </Box>
+
+
+                                        
+    // <>
+    // <VStack>
+    // <Stack display={'flex'} justifyContent='flex-start'>
+    //   <Text alignItems={"left"} position={"relative"} fontSize={"3xl"} fontWeight={300} textTransform={'uppercase'} mb={4}>Comments</Text>
+    // </Stack>
+    //   <VStack>
+    //     {posts?.map((post, index) => (
+    //       <Posts key={index} post={post} eventId={eventId} />
+    //     ))}
+    //     {/* {posts?.length ? (
+    //       <Box>
+    //         <Text>No Posts available</Text>
+    //       </Box>
+    //     ) : null} */}
+    //   </VStack>
+    // </VStack>
+    // </>
   );
 }
