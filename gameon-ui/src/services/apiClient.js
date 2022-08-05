7699c1ba18ea041f038154e0ce5da641cec6f5a9
Bosch <boschio91@gmail.com>
2022-08-05 16:01:21 -0400

    main-test revert to localhost

--- gameon-ui/src/services/apiClient.js
+++ gameon-ui/src/services/apiClient.js
@@ -120,5 +120,5 @@ class ApiClient {
 
 }
 
-// export default new ApiClient("http://localhost:3001")
-export default new ApiClient("https://git.heroku.com/gameon-labs.git")
+export default new ApiClient("http://localhost:3001")
+// export default new ApiClient("https://git.heroku.com/gameon-labs.git")
