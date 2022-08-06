f7673efcc9c65bef9abb73d64777db82042b0d8a
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-08-06 18:19:00 -0400

    Update Footer.jsx

--- gameon-ui/src/components/Footer/Footer.jsx
+++ gameon-ui/src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import {HashLink as Link } from 'react-router-hash-link'
-import { Container, Box, Text, Flex, Spacer, Heading, Stack } from "@chakra-ui/react"
+import { Container, Box, Text, Stack } from "@chakra-ui/react"
 import About from "../Footer/About"
 import { COLORS } from "../colors"
 // import ContactUs from "../Footer/ContactUs"
 
 export default function Footer(){
     return (
-            <Box paddingX={"10%"} fontFamily={"mono, sans-serif"}  backgroundColor={COLORS.indigo} color={COLORS.offWhite}>
+            <Box marginTop={"auto"} marginBottom={"auto"} paddingX={"10%"} fontFamily={"mono, sans-serif"}  backgroundColor={COLORS.indigo} color={COLORS.offWhite}>
                 {/* <Heading>THIS IS THE FOOTER</Heading> */}
                 <Container margin={0} as={Stack} maxWidth={"100%"} paddingY={4} direction={{ base: 'column', md: 'row' }} spacing={4}
                     justify={{ base: "center", md: "space-between" }} align={{ base: 'center', md: 'center' }}>
