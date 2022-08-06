d903c8ac1d355b3cf13ce497896df73423e56f45
AmberLs <93353341+AmJoy01@users.noreply.github.com>
2022-08-06 18:17:33 -0400

    Update About.jsx

--- gameon-ui/src/components/Footer/About.jsx
+++ gameon-ui/src/components/Footer/About.jsx
@@ -1,17 +1,25 @@
 import * as React from "react"
-import {Box, Heading, Container, SimpleGrid, Stack, StackDivider, Flex,  VStack, HStack, Image, Text, Highlight} from "@chakra-ui/react"
+import {Box, Heading, Container, Image, Text} from "@chakra-ui/react"
 import { COLORS } from "../colors"
 import pic from "../../contexts/media/Logo.png"
 // import { Link } from "react-router-dom"
 
 export default function About(){
     return (
-       <Box width={"100%"} height={"auto"} background={"hsla(255, 50%, 21%, 0.8)"} borderTopRadius={"20px"}>
+       <Box 
+            width={"100%"}
+            height={"100%"}
+            margin={"auto"}
+            display={"grid"}
+            gridTemplateColumns={"1fr"}
+            background={"hsla(255, 50%, 21%, 0.8)"}
+        >
+
         <Container centerContainer minWidth={"80%"}>
-        <Heading textAlign={"center"} marginLeft={2} paddingY={2} color={COLORS.offWhite} fontFamily={"Roboto, sans-serif"}>A gaming hub created for all gamers.</Heading>
-        <Image float={"left"} width={"300px"} marginBottom={-6} src={pic}/>
+        <Heading width={"100%"} margin={"auto"} marginX={6} paddingY={2} textAlign={"center"} color={COLORS.offWhite} fontFamily={"Roboto, sans-serif"}>A gaming hub created for all gamers.</Heading>
         <Box marginX={"20px"}>
-        <Text marginLeft={3} paddingBottom={2} whiteSpace={"pre-line"} fontFamily={"Open Sans, sans-serif"} fontSize={"xl"} color={COLORS.offWhite}>
+        <Text width={"50%"} margin={"auto"} paddingBottom={2} whiteSpace={"pre-line"} fontFamily={"Open Sans, sans-serif"} fontSize={"xl"} color={COLORS.offWhite}>
+        <Image float={"left"} width={"300px"} src={pic} marginRight={"-2rem"} marginBottom={"-5rem"}/>
         GameOn! is a community hub for all gamers looking to attend events. 
         Our mission is to have gamers of all ages and different backgrounds to attend these events. 
         Not only do we want gamers, but also event coordinators. 
